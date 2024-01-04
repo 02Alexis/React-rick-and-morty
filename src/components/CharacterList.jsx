bad0dced77b4485aa9f477ac5a3b230b8ac6013a
02Alexis <m_chael1012@hotmail.com>
2024-01-04 14:57:30 -0500

    creando navegacion

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,23 +1,41 @@
 import { useState, useEffect } from "react";
 import Character from "./Character";
 
+function NavPage(props) {
+  return (
+    <div className="d-flex justify-content-between align-items-center">
+      <p>Page {props.page}</p>
+      <button
+        onClick={() => props.setPage(props.page + 1)}
+        className="btn btn-primary btn-sm"
+      >
+        Page {props.page}
+      </button>
+    </div>
+  );
+}
+
 function CharacterList() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function getData() {
-      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
       const data = await response.json();
       setLoading(false);
       setCharacters(data.results);
     }
 
     getData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="container">
+      <NavPage page={page} setPage={setPage} />
       {loading ? (
         <h1>Loading</h1>
       ) : (
@@ -31,6 +49,7 @@ function CharacterList() {
           })}
         </div>
       )}
+      <NavPage page={page} setPage={setPage} />
     </div>
   );
 }
